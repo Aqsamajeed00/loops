7d0ccd58cd58f218901eeae7e1fc7c22a5256670
Aqsa Shaikh <160129099+Aqsamajeed00@users.noreply.github.com>
2024-06-09 15:18:55 +0500

    pushing my code to github

---
+++ main.js
@@ -0,0 +1,32 @@
+//While Loops
+let counter = 0;
+while (counter < 5) {
+    console.log(counter);
+    counter++;
+}
+;
+// EXAMPLE!!
+// Imagine you have to catch a bus in 5 minutes make a typescript while loop program for it
+let minuteinBus = 5;
+while (minuteinBus > 0) {
+    console.log(`Bus arrives in ${minuteinBus} minutes`);
+    minuteinBus--; //decrement
+}
+//While Loop
+let i = 0; // In this example the loop will continue to run as long as the condition 'i < 5' is true. The loop will stop when 'i' become equal to or greater than 5...
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+//For Loop...   
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+let person = ["Alice", 30];
+console.log();
+// Practice...
+let message1 = ["Apple", "Mango", "Watermelon"];
+console.log(message1);
+let message = ["Apple", "Mango", "Watermelon"];
+console.log(message[2]); //Output...Apple
+export {};
---
+++ main.ts
@@ -0,0 +1,33 @@
+//While Loops
+let counter = 0
+while (counter < 5){
+        console.log(counter)
+        counter++
+};
+
+// EXAMPLE!!
+// Imagine you have to catch a bus in 5 minutes make a typescript while loop program for it
+
+let minuteinBus = 5
+while(minuteinBus > 0) {
+        console.log(`Bus arrives in ${minuteinBus} minutes`)
+        minuteinBus--//decrement
+}
+
+//While Loop
+let i : number = 0;      // In this example the loop will continue to run as long as the condition 'i < 5' is true. The loop will stop when 'i' become equal to or greater than 5...
+while (i < 5 ){               //while loop condition p chlta hy 
+        console.log(i);
+        i ++
+}
+
+//For Loop...   
+for (let i : number = 0 ; i < 5 ; i ++) {
+        console.log (i);
+}
+  let person : [string,number] = ["Alice", 30];
+   console.log();
+
+
+
+
